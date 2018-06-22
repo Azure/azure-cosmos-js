fa8fb6315eb15f16e7ec1d4fabd9ea10c93b805f
Chris Anderson <chrande@microsoft.com>
2018-06-22 16:02:05 -0700

    Update query test

--- src/test/integration/query.spec.ts
+++ src/test/integration/query.spec.ts
@@ -2,6 +2,7 @@
 import { CosmosClient, UriFactory } from "../../";
 import testConfig from "./../common/_testConfig";
 import { TestHelpers } from "./../common/TestHelpers";
+import { PartitionKind } from "../../documents";
 
 const host = testConfig.host;
 const masterKey = testConfig.masterKey;
@@ -10,35 +11,33 @@ const doc = { id: "myId", pk: "pk" };
 
 describe("ResourceLink Trimming of leading and trailing slashes", function () {
     this.timeout(10000);
-    const client = new CosmosClient(host, { masterKey });
+    const client = new CosmosClient({ endpoint: host, auth: { masterKey } });
     const databaseId = "testDatabase";
     const collectionId = "testCollection";
 
-    afterEach(async function () { await TestHelpers.removeAllDatabases(host, masterKey); });
-    beforeEach(async function () { await TestHelpers.removeAllDatabases(host, masterKey); });
+    afterEach(async function () { await TestHelpers.removeAllDatabases(client); });
+    beforeEach(async function () { await TestHelpers.removeAllDatabases(client); });
 
     it("validate correct execution of query using named collection link with leading and trailing slashes"
         , async function () {
-            try {
-                const databaseBody = { id: databaseId };
+            const containerDefinition = {
+                id: collectionId,
+                partitionKey: {
+                    paths: ["/pk"],
+                    kind: PartitionKind.Hash,
+                },
+            };
+            const collectionOptions = { offerThroughput: 10100 };
 
-                const { result: database } = await client.createDatabase(databaseBody);
-                const collectionDefinition = { id: collectionId, partitionKey: { paths: ["/pk"], kind: "Hash" } };
-                const collectionOptions = { offerThroughput: 10100 };
+            const container = await TestHelpers.getTestContainer(
+                client, "validate correct execution of query", containerDefinition, collectionOptions);
 
-                const { result: createdCollection } = await client
-                    .createCollection(database._self, collectionDefinition, collectionOptions);
+            await container.items.create(doc);
+            const query = "SELECT * from " + collectionId;
+            const queryOptions = { partitionKey: "pk" };
+            const queryIterator = container.items.query(query, queryOptions);
 
-                const { result: docResult } = await client.createDocument(createdCollection._self, doc);
-                const collectionLink = "/dbs/" + databaseId + "/colls/" + collectionId + "/";
-                const query = "SELECT * from " + collectionId;
-                const queryOptions = { partitionKey: "pk" };
-                const queryIterator = client.queryDocuments(collectionLink, query, queryOptions);
-
-                const { result } = await queryIterator.toArray();
-                assert.equal(result[0]["id"], "myId");
-            } catch (err) {
-                throw err;
-            }
+            const { result } = await queryIterator.toArray();
+            assert.equal(result[0]["id"], "myId");
         });
 });
