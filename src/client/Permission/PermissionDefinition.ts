fcef52a6bcdb165e0314ffacbd3163d24f93ff42
Chris Anderson <chrande@microsoft.com>
2018-06-25 14:11:44 -0700

    authorization tests updated

--- src/client/Permission/PermissionDefinition.ts
+++ src/client/Permission/PermissionDefinition.ts
@@ -4,5 +4,5 @@ export interface PermissionDefinition {
     id?: string;
     permissionMode: PermissionMode;
     resource: string;
-    resourcePartitionKey?: string;
+    resourcePartitionKey?: string | any[]; // TODO: what's allowed here?
 }
