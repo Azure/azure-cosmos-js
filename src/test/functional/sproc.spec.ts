c7433f1fcd3727ad0478f3176566d7d8d53bdd31
Chris Anderson <chrande@microsoft.com>
2018-06-29 15:15:23 -0700

    fix sproc mistake from rename

--- src/test/functional/sproc.spec.ts
+++ src/test/functional/sproc.spec.ts
@@ -271,7 +271,7 @@ describe("NodeJS CRUD Tests", function () {
             id: "querySproc",
             body: function () {
                 var context = getContext();
-                var container = context.getcontainer();
+                var container = context.getContainer();
                 var response = context.getResponse();
 
                 // query for players
