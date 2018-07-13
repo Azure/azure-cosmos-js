a9227c6a20e3a7b80c7338a2f09d1ed4ee4976ef
Chris Anderson <chrande@microsoft.com>
2018-07-13 16:44:01 -0700

    Update Permissiono

---
+++ src/client/Permission/PermissionResponse.ts
@@ -0,0 +1,7 @@
+import { CosmosResponse } from "../../request";
+import { PermissionDefinition } from "./PermissionDefinition";
+import { Permission } from "./Permission";
+
+export interface PermissionResponse extends CosmosResponse<PermissionDefinition, Permission> {
+    permission: Permission;
+}
