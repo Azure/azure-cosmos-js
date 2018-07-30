cfa3fa1c84ad736ed0516a894109245c586b7142
Chris Anderson <chrande@microsoft.com>
2018-07-30 15:47:14 -0700

    Add ItemDefinition

--- src/client/Item/ItemResponse.ts
+++ src/client/Item/ItemResponse.ts
@@ -1,8 +1,9 @@
 import { CosmosResponse } from "../../request/CosmosResponse";
 import { Item } from "./Item";
 import { ItemBody } from "./ItemBody";
+import { ItemDefinition } from "./ItemDefinition";
 
-export interface ItemResponse<T> extends CosmosResponse<T & ItemBody, Item> {
+export interface ItemResponse<T extends ItemDefinition> extends CosmosResponse<T & ItemBody, Item> {
   /** Reference to the {@link Item} the response corresponds to. */
   item: Item;
 }
