de7aef0038049a73f7dd27dbc2a5aecc33e6e528
Chris Anderson <chrande@microsoft.com>
2018-08-21 19:25:55 -0700

    Add conflict types

--- src/client/Conflict/ConflictDefinition.ts
+++ src/client/Conflict/ConflictDefinition.ts
@@ -1,4 +1,10 @@
+import { ItemDefinition } from "../Item";
+
 export interface ConflictDefinition {
   /** The id of the conflict */
   id?: string;
+  sourceResourceId?: string;
+  conflictLSN?: string;
+  operationKind?: string; // TODO: enum
+  content?: ItemDefinition;
 }
