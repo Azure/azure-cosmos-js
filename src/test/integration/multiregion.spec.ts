7e03cd22c53a78006ff7469fabec2e20679c85e7
Christopher Anderson <chrande@microsoft.com>
2018-11-05 23:28:29 -0800

    variable names are hard

--- src/test/integration/multiregion.spec.ts
+++ src/test/integration/multiregion.spec.ts
@@ -7,7 +7,7 @@ import { endpoint, masterKey } from "../common/_testConfig";
 
 // This test requires a multi-region write enabled account with at least two regions.
 (process.env.TESTS_MULTIREGION ? describe : describe.skip)("Multi-region tests", function() {
-  let preferredRegions: string[] = [];
+  let preferredLocations: string[] = [];
   let dbAccount: DatabaseAccount;
 
   before(async function() {
@@ -15,20 +15,20 @@ import { endpoint, masterKey } from "../common/_testConfig";
     ({ body: dbAccount } = await client.getDatabaseAccount());
     // We reverse the order of the preferred locations list to make sure
     // we don't just follow the order we got back from the server
-    preferredRegions = dbAccount.readableLocations.map(v => v.name).reverse();
+    preferredLocations = dbAccount.readableLocations.map(v => v.name).reverse();
     assert(
-      preferredRegions.length > 1,
+      preferredLocations.length > 1,
       "Not a multi-region account. Please add a region before running this test again."
     );
   });
 
   it("Preferred locations should be honored for readEndpoint", async function() {
     const connectionPolicy = new ConnectionPolicy();
-    connectionPolicy.PreferredLocations = preferredRegions;
+    connectionPolicy.PreferredLocations = preferredLocations;
     const client = new CosmosClient({ endpoint, auth: { masterKey }, connectionPolicy });
-    const currentReadendpoint = await client.getReadEndpoint();
+    const currentReadEndpoint = await client.getReadEndpoint();
     assert(
-      currentReadendpoint.includes(preferredRegions[0].toLowerCase().replace(/ /g, "")),
+      currentReadEndpoint.includes(preferredLocations[0].toLowerCase().replace(/ /g, "")),
       "The readendpoint should be the first preferred location"
     );
   });
@@ -39,12 +39,12 @@ import { endpoint, masterKey } from "../common/_testConfig";
       "MultipleWriteableLocations must be set on your database account for this test to work"
     );
     const connectionPolicy = new ConnectionPolicy();
-    connectionPolicy.PreferredLocations = preferredRegions;
+    connectionPolicy.PreferredLocations = preferredLocations;
     connectionPolicy.UseMultipleWriteLocations = true;
     const client = new CosmosClient({ endpoint, auth: { masterKey }, connectionPolicy });
     const currentWriteEndpoint = await client.getWriteEndpoint();
     assert(
-      currentWriteEndpoint.includes(preferredRegions[0].toLowerCase().replace(/ /g, "")),
+      currentWriteEndpoint.includes(preferredLocations[0].toLowerCase().replace(/ /g, "")),
       "The writeendpoint should be the first preferred location"
     );
   });
