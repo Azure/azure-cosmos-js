68f2e7d44a2459bf7b6eeedf0993286b28fa57a1
Steve Faulkner <southpolesteve@gmail.com>
2018-11-29 23:33:15 -0500

    Loop over array for TS test. Opt out of latest via env var

--- ts-test.js
+++ ts-test.js
@@ -1,5 +1,11 @@
 const execa = require("execa");
 
+let versions = ["3.0", "3.1"];
+
+if (!process.env.SKIP_LATEST) {
+  versions.push("latest");
+}
+
 async function exec(cmd) {
   const command = execa.shell(cmd, { cwd: "./ts-test" });
   command.stderr.pipe(process.stderr);
@@ -10,13 +16,18 @@ async function exec(cmd) {
 (async () => {
   try {
     await exec("npm init -y");
+    console.log("Setting up typescript consumer project");
     await exec("npm install --save ./..");
-    await exec("npm install --save typescript@3.0");
-    await exec("tsc ./test.ts --allowSyntheticDefaultImports true");
-    await exec("npm install --save typescript@latest");
-    await exec("tsc ./test.ts --allowSyntheticDefaultImports true");
+    console.log("Installing @azure/cosmos as a file dependency");
+    for (const version of versions) {
+      console.log(`Installing typescript@${version}`);
+      await exec(`npm install --save typescript@${version}`);
+      console.log(`Compling with typescript@${version}`);
+      await exec(`tsc ./test.ts --allowSyntheticDefaultImports true`);
+    }
     process.exit(0);
   } catch (error) {
+    console.log("Typescript consumer test failed!");
     console.log(error);
     process.exit(1);
   }
---
+++ ts-test/test.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var Cosmos = require("@azure/cosmos");
+console.log(Object.keys(Cosmos));
