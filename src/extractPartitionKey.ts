4b64e18339a84964b0ed190d8f2c815a443d17a4
Steve Faulkner <southpolesteve@gmail.com>
2019-01-15 17:30:37 -0500

    Add files

---
+++ src/extractPartitionKey.ts
@@ -0,0 +1,23 @@
+import { parsePath } from "./common";
+import { PartitionKey, PartitionKeyDefinition } from "./documents";
+
+export function extractPartitionKey(document: any, partitionKeyDefinition: PartitionKeyDefinition): PartitionKey[] {
+  // TODO: any
+  if (partitionKeyDefinition && partitionKeyDefinition.paths && partitionKeyDefinition.paths.length > 0) {
+    const partitionKey: PartitionKey[] = [];
+    partitionKeyDefinition.paths.forEach((path: string) => {
+      const pathParts = parsePath(path);
+      let obj = document;
+      for (const part of pathParts) {
+        if (!(typeof obj === "object" && part in obj)) {
+          obj = {};
+          break;
+        }
+        obj = obj[part];
+      }
+      partitionKey.push(obj);
+    });
+    return partitionKey;
+  }
+  return undefined;
+}
