c8165b3367028d431092a68b9ba545994cbe793c
Christopher Anderson <chrande@microsoft.com>
2019-03-06 11:35:01 -0800

    Undo class refactor on FeedOptions

--- src/request/FeedOptions.ts
+++ src/request/FeedOptions.ts
@@ -3,45 +3,45 @@ import { CosmosHeaders } from "../index";
 /**
  * The feed options and query methods.
  */
-export class FeedOptions {
+export interface FeedOptions {
   /** Opaque token for continuing the enumeration. */
-  public continuation?: string;
+  continuation?: string;
   /**
    * DisableRUPerMinuteUsage is used to enable/disable Request Units(RUs)/minute capacity to serve
    * the request if regular provisioned RUs/second is exhausted.
    */
-  public disableRUPerMinuteUsage?: boolean;
+  disableRUPerMinuteUsage?: boolean;
   /**
    * A value indicating whether users are enabled to send more than one request to execute the query in the Azure Cosmos DB database service.
    *
    * More than one request is necessary if the query is not scoped to single partition key value.
    */
-  public enableCrossPartitionQuery?: boolean;
+  enableCrossPartitionQuery?: boolean;
   /** Allow scan on the queries which couldn't be served as indexing was opted out on the requested paths. */
-  public enableScanInQuery?: boolean;
+  enableScanInQuery?: boolean;
   /**
    * The maximum number of concurrent operations that run client side during parallel query execution in the
    * Azure Cosmos DB database service. Negative values make the system automatically decides the number of
    * concurrent operations to run.
    */
-  public maxDegreeOfParallelism?: number;
+  maxDegreeOfParallelism?: number;
   /** Max number of items to be returned in the enumeration operation. */
-  public maxItemCount?: number;
+  maxItemCount?: number;
   /** Specifies a partition key definition for a particular path in the Azure Cosmos DB database service. */
-  public partitionKey?: string;
+  partitionKey?: string;
   /** Token for use with Session consistency. */
-  public sessionToken?: string;
+  sessionToken?: string;
   /** (Advanced use case) Initial headers to start with when sending requests to Cosmos */
-  public initialHeaders?: CosmosHeaders;
+  initialHeaders?: CosmosHeaders;
   /** Indicates a change feed request. Must be set to "Incremental feed", or omitted otherwise. */
-  public useIncrementalFeed?: boolean;
+  useIncrementalFeed?: boolean;
   /** Conditions Associated with the request. */
-  public accessCondition?: {
+  accessCondition?: {
     /** Conditional HTTP method header type (IfMatch or IfNoneMatch). */
     type: string;
     /** Conditional HTTP method header value (the _etag field from the last version you read). */
     condition: string;
   };
   /** Enable returning query metrics in response headers */
-  public populateQueryMetrics?: boolean;
+  populateQueryMetrics?: boolean;
 }
