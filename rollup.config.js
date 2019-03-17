0125ca054facc62e08dc3bd22edafc3f355b9a14
Steve Faulkner <southpolesteve@gmail.com>
2019-03-16 22:28:11 -0700

    Convert to TS project references. Extract test folder

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,22 @@
 import resolve from "rollup-plugin-local-resolve";
-import json from "rollup-plugin-json";
-import multiEntry from "rollup-plugin-multi-entry";
+import replace from "rollup-plugin-replace";
+
+const version = require("./package.json").version;
 
 export default [
   {
-    input: "dist-esm/src/test/**/*.spec.js",
-    output: {
-      file: "dist-test/index.js",
-      format: "umd",
-      name: "Microsoft.Azure.CosmosTest",
-      sourcemap: true
-    },
-    plugins: [resolve(), multiEntry({ exports: false }), json()],
-    treeshake: false
-  },
-  {
-    input: "dist-esm/src/index.js",
+    input: "dist-esm/index.js",
     output: {
       file: "dist/index.js",
       format: "umd",
       name: "Microsoft.Azure.Cosmos",
       sourcemap: true
     },
-    plugins: [resolve(), json()]
+    plugins: [
+      resolve(),
+      replace({
+        REPALCE_WITH_SDK_VERSION: version
+      })
+    ]
   }
 ];
