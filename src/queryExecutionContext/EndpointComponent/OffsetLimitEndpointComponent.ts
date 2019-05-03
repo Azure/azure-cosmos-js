b388474fd91ba330ba44ccb3b2113c267cb6af01
Steve Faulkner <southpolesteve@gmail.com>
2019-05-02 21:10:14 -0400

    Remove comments

--- src/queryExecutionContext/EndpointComponent/OffsetLimitEndpointComponent.ts
+++ src/queryExecutionContext/EndpointComponent/OffsetLimitEndpointComponent.ts
@@ -4,19 +4,8 @@ import { getInitialHeader } from "../headerUtils";
 
 /** @hidden */
 export class OffsetLimitEndpointComponent implements ExecutionContext {
-  /**
-   * Represents an endpoint in handling top query. It only returns as many results as top arg specified.
-   * @constructor TopEndpointComponent
-   * @param { object } executionContext - Underlying Execution Context
-   * @ignore
-   */
   constructor(private executionContext: ExecutionContext, private limit: number, private offset: number) {}
 
-  /**
-   * Execute a provided function on the next element in the TopEndpointComponent.
-   * @memberof TopEndpointComponent
-   * @instance
-   */
   public async nextItem(): Promise<Response<any>> {
     if (this.offset > 0) {
       // Grab next item but ignore the result. We only need the headers
@@ -33,11 +22,6 @@ export class OffsetLimitEndpointComponent implements ExecutionContext {
     return { result: undefined, headers: getInitialHeader() };
   }
 
-  /**
-   * Retrieve the current element on the TopEndpointComponent.
-   * @memberof TopEndpointComponent
-   * @instance
-   */
   public async current(): Promise<Response<any>> {
     if (this.offset > 0) {
       const current = await this.executionContext.current();
@@ -46,12 +30,6 @@ export class OffsetLimitEndpointComponent implements ExecutionContext {
     return this.executionContext.current();
   }
 
-  /**
-   * Determine if there are still remaining resources to processs.
-   * @memberof TopEndpointComponent
-   * @instance
-   * @returns {Boolean} true if there is other elements to process in the TopEndpointComponent.
-   */
   public hasMoreResults() {
     return (this.offset > 0 || this.limit > 0) && this.executionContext.hasMoreResults();
   }
