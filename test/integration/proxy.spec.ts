49525c812fce794db707ae6f026273de5100de92
Steve Faulkner <southpolesteve@gmail.com>
2019-05-31 10:43:43 -0400

    Remove node builtin usage in favor of universal packages/APIs

--- test/integration/proxy.spec.ts
+++ test/integration/proxy.spec.ts
@@ -1,7 +1,6 @@
 ﻿import * as http from "http";
 import * as net from "net";
 import ProxyAgent from "proxy-agent";
-import * as url from "url";
 import { CosmosClient } from "../../dist-esm/index";
 import { endpoint, masterKey } from "../common/_testConfig";
 import { addEntropy } from "../common/TestHelpers";
@@ -15,7 +14,7 @@ if (!isBrowser()) {
     });
 
     proxy.on("connect", (req, clientSocket, head) => {
-      const serverUrl = url.parse(`http://${req.url}`);
+      const serverUrl = new URL(`http://${req.url}`);
       const serverSocket = net.connect(
         parseInt(serverUrl.port, 10),
         serverUrl.hostname,
