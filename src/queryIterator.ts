ddc07daa7279cb74fd280ae32181f7b9fb2a0ed2
Christopher Anderson <chrande@microsoft.com>
2019-06-06 16:20:38 -0700

    undo query changes

--- src/queryIterator.ts
+++ src/queryIterator.ts
@@ -1,6 +1,5 @@
 /// <reference lib="esnext.asynciterable" />
 import { ClientContext } from "./ClientContext";
-import { generateGuidId, OperationType, ResourceType } from "./common";
 import {
   CosmosHeaders,
   FetchFunctionCallback,
@@ -10,10 +9,8 @@ import {
   ProxyQueryExecutionContext,
   SqlQuerySpec
 } from "./queryExecutionContext";
-import { isErrorResponse } from "./request/ErrorResponse";
 import { FeedOptions } from "./request/FeedOptions";
 import { FeedResponse } from "./request/FeedResponse";
-import { InternalOperationStats } from "./request/OperationStatistics";
 
 /**
  * Represents a QueryIterator Object, an implmenetation of feed or query response that enables
@@ -32,8 +29,7 @@ export class QueryIterator<T> {
     private query: SqlQuerySpec | string,
     private options: FeedOptions,
     private fetchFunctions: FetchFunctionCallback | FetchFunctionCallback[],
-    private resourceLink?: string,
-    private resourceType?: ResourceType
+    private resourceLink?: string
   ) {
     this.query = query;
     this.fetchFunctions = fetchFunctions;
@@ -147,30 +143,10 @@ export class QueryIterator<T> {
    * before returning the first batch of responses.
    */
   public async fetchNext(): Promise<FeedResponse<T>> {
-    const opstats = new InternalOperationStats(
-      this.resourceType,
-      OperationType.Query,
-      this.resourceLink,
-      generateGuidId()
-    );
     try {
       const response = await this.queryExecutionContext.fetchMore();
-      opstats.mergeChildOperation(response.operationStatistics);
-      opstats.complete();
-      return new FeedResponse<T>(
-        response.result,
-        response.headers,
-        this.queryExecutionContext.hasMoreResults(),
-        opstats
-      );
+      return new FeedResponse<T>(response.result, response.headers, this.queryExecutionContext.hasMoreResults());
     } catch (err) {
-      opstats.fail();
-      if (isErrorResponse(err)) {
-        if (err.operationStatistics) {
-          opstats.mergeChildOperation(err.operationStatistics as InternalOperationStats);
-        }
-        err.operationStatistics = opstats;
-      }
       throw err;
     }
   }
--- src/request/FeedResponse.ts
+++ src/request/FeedResponse.ts
@@ -1,13 +1,11 @@
 import { Constants } from "../common";
 import { CosmosHeaders } from "../queryExecutionContext";
-import { OperationStats } from "./OperationStatistics";
 
 export class FeedResponse<TResource> {
   constructor(
     public readonly resources: TResource[],
     private readonly headers: CosmosHeaders,
-    public readonly hasMoreResults: boolean,
-    public readonly operationStats?: OperationStats
+    public readonly hasMoreResults: boolean
   ) {}
   public get continuation(): string {
     return this.headers[Constants.HttpHeaders.Continuation];
