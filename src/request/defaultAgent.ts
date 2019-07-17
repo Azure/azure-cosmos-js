2f3982d57c9f58d009bfe068a6c0768f38bf2099
Steve Faulkner <southpolesteve@gmail.com>
2019-07-17 12:35:29 -0500

    Try different module for browser

--- src/request/defaultAgent.ts
+++ src/request/defaultAgent.ts
@@ -11,13 +11,13 @@ export let defaultHttpAgent: Agent;
 export let defaultHttpsAgent: Agent;
 
 if (isNode) {
-  // tslint:disable-next-line:no-eval
-  const https = eval("require")("https");
+  // tslint:disable-next-line:no-var-requires
+  const https = require("https");
   defaultHttpsAgent = new https.Agent({
     keepAlive: true
   });
-  // tslint:disable-next-line:no-eval
-  const http = eval("require")("http");
+  // tslint:disable-next-line:no-var-requires
+  const http = require("http");
   defaultHttpAgent = new http.Agent({
     keepAlive: true
   });
