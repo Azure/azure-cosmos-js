0717a19fea3f00773fb1a868afc1975393f99ded
Steve Faulkner <southpolesteve@gmail.com>
2019-07-22 19:06:28 -0500

    Typo

--- src/request/FeedOptions.ts
+++ src/request/FeedOptions.ts
@@ -9,7 +9,7 @@ export interface FeedOptions extends SharedOptions {
   /**
    * Continuation Tokens contain optional data that can be removed from the serialization before writing it out to a header.
    * By default we are capping this to 1kb to avoid long headers (Node.js has a global header size limit).
-   * A user may set this field to allow for longer headers, which can help the backend optimize during query execution."
+   * A user may set this field to allow for longer headers, which can help the backend optimize query execution."
    */
   continuationTokenLimitInKB?: number;
   /** Allow scan on the queries which couldn't be served as indexing was opted out on the requested paths. */
